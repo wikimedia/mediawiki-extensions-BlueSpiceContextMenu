f4a8ca7252abc31331695b9e02388b70596b6b32
hslater <hamish.slater@hallowelt.com>
2025-01-23 16:28:20 +0100

    Update Config namespace
    ERM38444
    
    [REL1_43, master]

--- src/Hook/BsContextMenuGetItems.php
+++ src/Hook/BsContextMenuGetItems.php
@@ -27,6 +27,7 @@
 
 namespace BlueSpice\ContextMenu\Hook;
 
+use MediaWiki\Config\Config;
 use MediaWiki\Title\Title;
 
 abstract class BsContextMenuGetItems extends \BlueSpice\Hook {
@@ -63,7 +64,7 @@ abstract class BsContextMenuGetItems extends \BlueSpice\Hook {
 
 	/**
 	 * @param \IContextSource $context
-	 * @param \Config $config
+	 * @param Config $config
 	 * @param array &$items
 	 * @param Title $title
 	 */
